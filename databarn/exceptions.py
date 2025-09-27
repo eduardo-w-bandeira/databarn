41e051f4b4e4d69e12b5b2d975b1e91a9744c4e7
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2025-09-27 13:16:14 -0700

    Refactor: Update exception hierarchy by replacing DataBarnError with DataBarnViolationError in exception classes; enhance attribute handling in Grain and Seed classes.

--- databarn/exceptions.py
+++ databarn/exceptions.py
@@ -1,12 +1,12 @@
-class DataBarnError(Exception):
+class DataBarnViolationError(Exception):
     """Base class for all exceptions raised by DataBarn."""
     pass
 
-class DataBarnSyntaxError(DataBarnError, SyntaxError):
+class DataBarnSyntaxError(DataBarnViolationError, SyntaxError):
     """Raised when an operation violates the syntax rules of DataBarn."""
     pass
 
-class CobConsistencyError(DataBarnError):
+class CobConsistencyError(DataBarnViolationError):
     """Raised when an operation would lead to an inconsistent state in the Cob.
     This is a generic class for uncategorized Cob consistency errors."""
     pass
@@ -23,10 +23,10 @@ class GrainTypeMismatchError(ConstraintViolationError, TypeError):
     """Raised when an assignment to a grain is of the wrong type."""
     pass
 
-class InvalidGrainLabelError(DataBarnError, NameError):
+class InvalidGrainLabelError(DataBarnViolationError, NameError):
     """Raised when a variable name is invalid, and cannot be used as a grain label."""
     pass
 
-class BarnConsistencyError(DataBarnError, ValueError, TypeError):
+class BarnConsistencyError(DataBarnViolationError, ValueError, TypeError):
     """Raised when an operation would lead to an inconsistent state in the Barn."""
     pass
--- databarn/grain.py
+++ databarn/grain.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
+from typing import Any, Type, get_type_hints
 from .trails import sentinel
-from typing import Any, Type
+from .exceptions import DataBarnViolationError
+
+class _CustomGrainAttrs:
+    """A mixin class to allow custom attributes on Grain."""
+
+    def __init__(self, **custom_attrs):
+        self.__dict__.update(custom_attrs)
 
 
 class Grain:
     """Cob-Model Grain: Grain definition for the Cob-like class."""
-    label: str # This will be set in the Cob-model dna
+    label: str
+    type: Type | None
     default: Any
     pk: bool
     auto: bool
@@ -14,15 +22,15 @@ class Grain:
     unique: bool
     comparable: bool
     key_name: str
-    type: Any # This will be set in the Cob-model dna
-    model: Type # This will be set in the Cob-model dna
+    model: Type["Cob"] | None
     pre_value: Any
+    custom: _CustomGrainAttrs
 
-    def __init__(self, default: Any = None, pk: bool = False, auto: bool = False,
-                 required: bool = False, frozen: bool = False, unique: bool = False,
-                 comparable: bool = False, key_name: str="", **custom_attrs):
+    def __init__(self, default: Any = None, pk: bool = False, required: bool = False,
+                 auto: bool = False, frozen: bool = False, unique: bool = False,
+                 comparable: bool = False, key_name: str = "", **custom_attrs):
         """Initialize the Grain object.
-        
+
         Args:
             default: The default value of the grain.
             pk: Whether this grain is part of the primary key.
@@ -37,17 +45,19 @@ class Grain:
                 If not provided, the label will be used.
             custom_attrs: Any additional custom attributes to set on the Grain object.
         """
-        self.label = ""  # Placed here to show up first in repr.
+        self.label = ""  # This will be set in the Cob-model dna
+        self.type = None  # This will be set in the Cob-model dna
         self.default = default
         self.pk = pk
+        self.required = required
         self.auto = auto
         self.frozen = frozen
-        self.required = required
         self.unique = unique
         self.comparable = comparable
         self.key_name = key_name
+        self.model = None  # This will be set in the Cob-model dna
         self.pre_value = sentinel
-        self.__dict__.update(custom_attrs)
+        self.custom = _CustomGrainAttrs(**custom_attrs)
 
     def _set_model_attrs(self, model: Type, label: str, type: Any) -> None:
         self.model = model
@@ -56,7 +66,7 @@ class Grain:
 
     def set_key_name(self, key_name: str) -> None:
         """Set the key_name attribute.
-        
+
         This method can be used on the fly, but should be done with care,
         preferably before the cob object is used.
         """
@@ -84,8 +94,9 @@ class Grain:
 class Seed:
 
     # Cob-object specific attributes
-    cob: "Cob" # Bound cob object
-    has_been_set: bool
+    cob: "Cob"  # Bound cob object
+    has_been_set: bool # @property
+    grain: Grain  # Bound grain object
 
     def __init__(self, cob: "Cob", grain: Grain):
         """Initialize the Seed object.
@@ -95,7 +106,7 @@ class Seed:
         """
         for name, value in grain.__dict__.items():
             # To show up in repr(), dir(), help(), etc.
-            setattr(self, name, value)
+            object.__setattr__(self, name, value)
         self.cob = cob
         self.grain = grain
 
@@ -103,18 +114,21 @@ class Seed:
         grain = super().__getattribute__('grain')
         if not name.startswith('_') and name in grain.__dict__:
             value = getattr(grain, name)
-            setattr(self, name, value)
+            object.__setattr__(self, name, value)  # Cache it for caution
             return value
         return super().__getattribute__(name)
+
+    def __setattr__(self, name, value):
+        raise DataBarnViolationError("Setting attributes on Seed is not allowed.")
 
     def get_value(self) -> Any:
         """Get the value of the grain at the given moment."""
         return getattr(self.cob, self.label)
-    
+
     def set_value(self, value: Any) -> None:
         """Set the value of the grain in the cob."""
         setattr(self.cob, self.label, value)
-    
+
     def force_set_value(self, value: Any) -> None:
         """Force set the value of the grain, bypassing any checks.
 
@@ -136,8 +150,14 @@ class Seed:
 
         F.ex.:
             Seed(label='number', type=int, default=0, pk=False, auto=False,
-            frozen=False, none=True)"
+            frozen=False, required=True)"
         """
-        items = [f"{k}={v!r}" for k, v in self.__dict__.items()]
+        map = self.grain.__dict__.copy()
+        for key, value in self.__dict__.items():
+            if key not in map:
+                map[key] = value
+        has_been_set_name = self.has_been_set.__name__
+        map[has_been_set_name] = self.has_been_set
+        items = [f"{k}={v!r}" for k, v in map.items()]
         sep_items = ", ".join(items)
         return f"{type(self).__name__}({sep_items})"
