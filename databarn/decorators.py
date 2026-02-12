f88bf72825acc4b1c45eaa1c11b2cbae2b6a187a
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2026-02-12 07:50:54 -0800

    Remove magical label creation in decorators

--- databarn/decorators.py
+++ databarn/decorators.py
@@ -1,13 +1,13 @@
 from typing import Type
 import typeguard
-from .trails import pascal_to_underscore, fo
+from .trails import fo
 from .barn import Barn
 from .cob import Cob
 from .grain import Grain
 from .exceptions import DataBarnSyntaxError
 
 @typeguard.typechecked
-def one_to_many_grain(label: str = "", frozen: bool = True, **grain_kwargs):
+def one_to_many_grain(label: str, **grain_kwargs):
     """Defines a sub-Barn Grain based on the given Cob-model.
 
     - Once this decorator is applied, the outer Cob will wizardly create a Grain() of
@@ -17,39 +17,29 @@ def one_to_many_grain(label: str = "", frozen: bool = True, **grain_kwargs):
     - It's up the user to add instances of the decorated Cob-model class to the Barn.
 
     Args:
-        label (str): The label of the grain. If not provided,
-            it is generated from the class name in underscore_case
-            and pluralized by adding 's' if it doesn't already end with 's'.
-        frozen (bool): Whether the grain is frozen (immutable) after being set once.
-            Default is True.
+        label (str): The label of the grain.
         grain_kwargs: Kwargs to be passed to the Grain constructor.
 
     Returns:
         A decorator that sets the Cob-model as a sub-Barn grain.
     """
     # The decorator function that will be applied to the child Cob-like class
+    @typeguard.typechecked
     def decorator(child_model: Type[Cob]):
-        if not issubclass(child_model, Cob):
-            raise DataBarnSyntaxError("The decorated class must be a subclass of Cob.")
         if child_model.__dna__.dynamic:
             raise DataBarnSyntaxError(fo(f"""
                 Dynamic Cob-models cannot be used as child models in a Barn grain.
                 You must define at least one Grain in '{child_model.__name__}',
                 in order for it to be a static Cob-model."""))
-        nonlocal label
-        if not label:
-            label = pascal_to_underscore(child_model.__name__)
-            label += "s" if not label.endswith("s") else ""
-        grain = Grain(
-            frozen=frozen, factory=child_model.__dna__.create_barn, **grain_kwargs)
-        grain._set_parent_model_metadata(parent_model=None, label=label, type=Barn)
+        grain = Grain(factory=child_model.__dna__.create_barn, **grain_kwargs)
+        grain._set_parent_model_metadata(parent_model=None, label=label, type=Barn[child_model])
         grain._set_child_model(child_model, is_child_barn=True)
         child_model.__dna__._set_outer_model_grain(grain)
         return child_model
     return decorator
 
 @typeguard.typechecked
-def one_to_one_grain(label: str = "", **grain_kwargs):
+def one_to_one_grain(label: str, **grain_kwargs):
     """Defines a sub-Cob grain based on the given Cob-model.
 
     - Once this decorator is applied, the outer Cob-model will wizardly create a Grain() of
@@ -59,8 +49,7 @@ def one_to_one_grain(label: str = "", **grain_kwargs):
     - It's up the user to set the value to an instance of the decorated Cob-model.
 
     Args:
-        label (str): The label of the grist. If not provided,
-            it is generated from the class name in underscore_case.
+        label (str): The label of the grist.
         grain_kwargs: Kwargs to be passed to the Grain constructor.
 
     Returns:
@@ -76,9 +65,6 @@ def one_to_one_grain(label: str = "", **grain_kwargs):
                 Dynamic Cob-models cannot be used as child models in a Cob Grain.
                 You must define at least one Grain in '{child_model.__name__}',
                 in order for it to be a static Cob-model."""))
-        nonlocal label
-        if not label:
-            label = pascal_to_underscore(child_model.__name__)
         grain._set_parent_model_metadata(parent_model=None, label=label, type=child_model)
         grain._set_child_model(child_model, is_child_barn=False)
         child_model.__dna__._set_outer_model_grain(grain)
