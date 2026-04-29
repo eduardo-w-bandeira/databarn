16ea75fecdfd43d8c7d72a19134002a149533ad3
Eduardo Bandeira <78479953+eduardo-w-bandeira@users.noreply.github.com>
2026-04-28 23:56:19 -0700

    Bump version to 1.10.0

--- databarn/__init__.py
+++ databarn/__init__.py
@@ -21,7 +21,7 @@ from .exceptions import (
     GrainTypeMismatchError, GrainLabelError,
     BarnConstraintViolationError)
 
-__version__ = "1.9.2"
+__version__ = "1.10.0"
 
 __all__ = [
     "Barn",
